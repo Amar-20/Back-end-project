e1ae0c9af482fca73f1be3f46d19f132a3af834e
Amar-20 <konangeri1720@gmail.com>
2024-05-17 12:56:45 -0400

    database connection

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,7 @@ import { DB_NAME } from '../constant.js';
 
 const connectDB = async()=>{
     try {
+        //mongoose gives a return object after connecting. we are holding that responses in variable called connectionInstance.
         const connectionInstance = await mongoose.connect(`${process.env.MONGDB_URL}/${DB_NAME}`)
         console.log(`\n Mongodb connected!! DB_Host: ${connectionInstance.connection.host}`) // to know to which server we have connected.(connection.host)
     } catch (error) {
