dc4eff8b4fcc2e91c57736429ecaca13a3259e33
Amar-20 <konangeri1720@gmail.com>
2024-05-20 13:09:39 -0400

    user_register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,21 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:'Code with Amar!'
-    })
-    
+    // res.status(200).json({
+    //     message:'Code with Amar!'
+    // })
+
+    //1)get user details from frontend
+    const {fullname,email,username,password}=req.body
+    console.log(email)
+    //2)validation
+    //3)check if user is already exists:-username,email
+    //4)check for images,check for avatar
+    //5)//upload them to clouinary(utils), avatar
+    //6)create user object - create entry in db
+    //7)remove password and refresh token field from response.
+    //8)check for user creation
+    //return res.
 })
 
 export {registerUser}
