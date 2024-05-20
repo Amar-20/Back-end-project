6602639ee94c4ebe7ff03b10a6365e75d7502d13
Amar-20 <konangeri1720@gmail.com>
2024-05-20 09:38:39 -0400

    Express

--- src/app.js
+++ src/app.js
@@ -8,8 +8,11 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
+// we can get request in many forms(form,json,url,body etc.,)so we define config and limit.
 app.use(express.json({limit:'16kb'}))
+// config url 
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
+// to store files temp.
 app.use(express.static('public'))
 // We can safely access the clients web browser cookies, perform some opeations on it and aslo safely put some cookies.
 app.use(cookieParser())
